c118a76745fc277c3d9dc96caeb9a4db9896ec88
Yota Toyama <raviqqe@gmail.com>
2025-08-28 08:04:18 -0700

    Fix

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/languages/*.ts", "src/themes/*.ts"],
+  entry: [
+    "src/index.ts",
+    "src/pattern.ts",
+    "src/token.ts",
+    "src/languages/*.ts",
+    "src/themes/*.ts",
+  ],
   minify: true,
   platform: "browser",
 });
