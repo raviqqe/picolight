0fbb35eb8ae13aeea8a7559275862c65025cc442
Yota Toyama <raviqqe@gmail.com>
2025-08-27 19:11:58 -0700

    Fix

--- src/languages/scheme.ts
+++ src/languages/scheme.ts
@@ -1,3 +1,4 @@
+import { string } from "../lexer.js";
 import type { Pattern } from "../pattern.js";
 
-export const scheme: Pattern[] = ;
+export const scheme: Pattern[] = [[string, (foo) => foo]];
