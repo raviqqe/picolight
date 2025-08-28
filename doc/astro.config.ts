75a78b8970894c4bc75f5cc999552629ef55cecb
Yota Toyama <raviqqe@gmail.com>
2025-08-28 08:25:08 -0700

    fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -1,16 +1,8 @@
 import mdx from "@astrojs/mdx";
-import preact from "@astrojs/preact";
-import react from "@astrojs/react";
-import solid from "@astrojs/solid-js";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  base: "/monza-editor",
-  integrations: [
-    mdx(),
-    preact({ include: "**/preact/**/*.tsx" }),
-    react({ include: "**/react/**/*.tsx" }),
-    solid({ include: "**/solid/**/*.tsx" }),
-  ],
-  site: "https://raviqqe.com/monza-editor",
+  base: "/picolight",
+  integrations: [mdx()],
+  site: "https://raviqqe.com/picolight",
 });
