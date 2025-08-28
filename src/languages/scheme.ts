01a47e0ed9973f65c6eed0e04a5096496d7a3848
Yota Toyama <raviqqe@gmail.com>
2025-08-27 19:22:13 -0700

    Fix

--- src/languages/scheme.ts
+++ src/languages/scheme.ts
@@ -1,8 +1,8 @@
 import * as category from "../category.js";
-import { parenthesis, string } from "../pattern.js";
+import * as pattern from "../pattern.js";
 import type { Language } from "../pattern.js";
 
 export const scheme: Language = [
-  [string, category.string],
-  [parenthesis, category.punctuation],
+  [pattern.string, category.string],
+  [pattern.parenthesis, category.punctuation],
 ];
