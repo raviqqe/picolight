feaae8a461298f58f7a0f2e4e83337c651338109
Yota Toyama <raviqqe@gmail.com>
2025-08-27 18:04:00 -0700

    Refactor

--- src/lexer.ts
+++ src/lexer.ts
@@ -6,3 +6,6 @@ export const string = (text: string): number | null =>
 
 export const blank = (text: string): number | null =>
   result(/( \t\r\n)*/.exec(text));
+
+export const punctuation = (text: string): number | null =>
+  result(/[\[\](){}]/.exec(text));
