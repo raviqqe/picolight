148557df02f62c203bdb5eca47df9225997044fb
Yota Toyama <raviqqe@gmail.com>
2025-08-27 18:29:50 -0700

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 export const highlight = (
   text: string,
   patterns: [RegExp, (text: string) => Element][],
-): string => {
-  for (const [pattern, convert] of patterns) {
-    const index = pattern.exec(text)?.index;
+): HTMLElement => {
+  const code =document.createElement("code");
 
-    if (foo) {
+  while (text) {
+    for (const [pattern, convert] of patterns) {
+      const index = pattern.exec(text)?.index;
+
+      if (index) {
+        code.appendChild(document.createTextNode(text.slice(0, index)));
+        text= text.slice(index)
+        break;
+      }
+
     }
 
-    text = text.slice(index ?? 0);
+      text = text.slice(index ?? 0);
   }
 
-  return text;
+  return code;
 };
