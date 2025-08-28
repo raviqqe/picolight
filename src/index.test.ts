602c74bb1e4ab52a223d5490f5a87cd4e19837b2
Yota Toyama <raviqqe@gmail.com>
2025-08-27 19:29:54 -0700

    Fix

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,7 @@
 import { expect, it } from "vitest";
 import { highlight } from "./index.js";
+import { githubDark } from "./themes/github-dark.js";
 
 it("highlights code", () => {
-  expect(highlight("foo", []).textContent).toMatchSnapshot();
+  expect(highlight("foo", [], githubDark).textContent).toMatchSnapshot();
 });
---
+++ src/themes/github-dark.ts
@@ -0,0 +1,3 @@
+import type { Theme } from "../theme.js";
+
+export const githubDark: Theme = {};
