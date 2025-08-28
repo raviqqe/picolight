77f4ae1f56959bcfc12aa74e46c6a5ddb551e5b1
Yota Toyama <raviqqe@gmail.com>
2025-08-27 20:56:15 -0700

    Fix

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,8 @@
 import { expect, it } from "vitest";
 import { highlight } from "./index.js";
 import { picoDark } from "./themes/pico-dark.js";
+import { scheme } from "./languages/scheme.js";
 
 it("highlights code", () => {
-  expect(highlight("foo", [], picoDark).outerHTML).toMatchSnapshot();
+  expect(highlight("foo", scheme, picoDark).outerHTML).toMatchSnapshot();
 });
