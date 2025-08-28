9636cb2100693560edfe8e70b102b7aca17c8e21
Yota Toyama <raviqqe@gmail.com>
2025-08-27 20:33:22 -0700

    Fix

--- src/themes/pico-dark.ts
+++ src/themes/pico-dark.ts
@@ -1,5 +1,5 @@
 import type { Theme } from "../theme.js";
-import { number, string } from "../token.js";
+import { number, punctuation, string } from "../token.js";
 
 export const picoDark: Theme = {
   [number]: [null, "lightyellow"],
