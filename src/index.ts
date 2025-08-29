027af8eeb00d6946c163ecc0bf76a331833e06bf
Yota Toyama <raviqqe@gmail.com>
2025-08-28 21:05:21 -0700

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { highlight } from "./highlight.js";
 export type { Language, Lexer } from "./language.js";
 export type { Tag, Theme } from "./theme.js";
+export type { Token } from "./token.js";
