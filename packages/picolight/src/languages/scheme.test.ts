4f55ee76a757125f672f95df82aab42ca6651bcf
Yota Toyama <raviqqe@gmail.com>
2025-08-30 14:28:38 -0700

    fix

--- packages/picolight/src/languages/scheme.test.ts
+++ packages/picolight/src/languages/scheme.test.ts
@@ -3,5 +3,12 @@ import { lex } from "../highlight.js";
 import { scheme } from "./scheme.js";
 
 it("matches a comment", () => {
-  expect(lex("#|||#", scheme.lexers)).toEqual([["comment"], "#|||#"]);
+  const source = "#|||#";
+  expect(lex(source, scheme.lexers)).toEqual([["comment"], source]);
+});
+
+it("matches a multi-line comment", () => {
+  const source = "#|foo\nbar\nbaz|#";
+
+  expect(lex(source, scheme.lexers)).toEqual([["comment"], source]);
 });
