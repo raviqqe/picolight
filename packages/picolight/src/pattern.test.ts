2034c7c8e462c0f69434972fbfc7b3bb16703a44
Yota Toyama <raviqqe@gmail.com>
2025-08-30 11:33:09 -0700

    Test more patterns (#44)

--- packages/picolight/src/pattern.test.ts
+++ packages/picolight/src/pattern.test.ts
@@ -1,9 +1,41 @@
 import { describe, expect, it } from "vitest";
-import { string } from "./pattern.js";
+import { number, parenthesis, string } from "./pattern.js";
+
+describe("number", () => {
+  it("matches an integer", () => {
+    const text = "42";
+
+    expect(text.match(number)?.[0]).toBe(text);
+  });
+
+  it("matches a positive sign", () => {
+    const text = "+42";
+
+    expect(text.match(number)?.[0]).toBe(text);
+  });
+
+  it("matches a negative sign", () => {
+    const text = "-42";
+
+    expect(text.match(number)?.[0]).toBe(text);
+  });
+
+  it("matches an exponent", () => {
+    const text = "42e42";
+
+    expect(text.match(number)?.[0]).toBe(text);
+  });
+});
 
 describe("string", () => {
   it("matches a string", () => {
     const text = `"foo"`;
+
+    expect(text.match(string)?.[0]).toBe(text);
+  });
+
+  it("matches escapes", () => {
+    const text = `"\\n\\r\\t\\"\\\\"`;
 
     expect(text.match(string)?.[0]).toBe(text);
   });
@@ -13,4 +45,18 @@ describe("string", () => {
 
     expect(text.match(string)?.[0]).toBe(text);
   });
+
+  it("matches a multi-line string to the end", () => {
+    const text = '"foo\nbar';
+
+    expect(text.match(string)?.[0]).toBe(text);
+  });
+});
+
+describe("parenthesis", () => {
+  for (const text of "(){}[]") {
+    it(`matches ${text}`, () => {
+      expect(text.match(parenthesis)?.[0]).toBe(text);
+    });
+  }
 });
