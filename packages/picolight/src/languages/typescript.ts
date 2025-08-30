9a1db21bbbca4e29690549523ddfc1b25a57c663
Yota Toyama <raviqqe@gmail.com>
2025-08-30 10:04:49 -0700

    Fix

--- packages/picolight/src/languages/typescript.ts
+++ packages/picolight/src/languages/typescript.ts
@@ -55,7 +55,7 @@ export const typescript: Language = {
       ),
       ["keyword"],
     ],
-    [/^\/\*([^*]|[*][^/])*\*\//ms, ["comment"]],
+    [/^\/\*([^*]|\*[^/])*\*\//ms, ["comment"]],
     [/^\/\/[^\n]*\n/, ["comment"]],
   ],
 };
