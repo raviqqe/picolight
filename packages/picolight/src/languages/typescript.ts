48d793524f10e2b53cf9c62a4e9a78e9b49e2bd3
Yota Toyama <raviqqe@gmail.com>
2025-08-29 23:13:19 -0700

    Fix

--- packages/picolight/src/languages/typescript.ts
+++ packages/picolight/src/languages/typescript.ts
@@ -1,14 +1,37 @@
 import type { Language } from "../language.js";
 import { keyword, number, parenthesis, string } from "../pattern.js";
 
-export const scheme: Language = {
+const identifierLetter = "[A-Z-az0-9_]";
+
+export const typescript: Language = {
   lexers: [
     [/^(false|true)(?![A-Za-z0-9])/, ["constant"]],
     [number, ["number", "constant"]],
     [string, ["string"]],
     [parenthesis, ["punctuation"]],
-    [keyword(["import"]), ["variable"]],
-    [keyword(["import"]), ["keyword"]],
+    [
+      keyword(
+        [
+          "async",
+          "await",
+          "const",
+          "do",
+          "export",
+          "else",
+          "type",
+          "for",
+          "from",
+          "function",
+          "if",
+          "import",
+          "let",
+          "var",
+          "while",
+        ],
+        identifierLetter,
+      ),
+      ["keyword"],
+    ],
     [/^#;/, ["punctuation"]],
     [/^;[^\n]*\n/s, ["comment"]],
   ],
