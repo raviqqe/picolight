e7c1182458258b977c4d33d0542ad46cd57734ac
Yota Toyama <raviqqe@gmail.com>
2025-08-30 15:05:20 -0700

    Fix

--- packages/picolight/src/pattern.ts
+++ packages/picolight/src/pattern.ts
@@ -13,4 +13,4 @@ export const identifier = (head: string, tail: string) =>
   new RegExp(`^${head}${tail}*`);
 
 export const surround = (start: RegExp, middle: RegExp, end: RegExp): RegExp =>
-  new RegExp(`^${start}(${middle})*${end}`);
+  new RegExp(`^${start.source}(${middle.source})*${end.source}`);
