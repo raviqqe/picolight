a4a7565a12c0d3e409ac007611b8004fbf36af40
Yota Toyama <raviqqe@gmail.com>
2025-08-29 19:26:46 -0700

    Fix

--- packages/picolight/src/pattern.ts
+++ packages/picolight/src/pattern.ts
@@ -1,5 +1,7 @@
 export const keyword = (keywords: string[]) =>
-  new RegExp(`^(${keywords.map(RegExp.escape).join("|")})`);
+  new RegExp(
+    `^(${keywords.map((RegExp as unknown as { escape: (text: string) => string }).escape).join("|")})`,
+  );
 
 export const number = /^[+-]?[0-9.]+/;
 
