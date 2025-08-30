0e5c403ae66493c981812b790928c18838fa5e78
Yota Toyama <raviqqe@gmail.com>
2025-08-30 14:10:00 -0700

    fix

--- packages/picolight/src/languages/typescript.test.ts
+++ packages/picolight/src/languages/typescript.test.ts
@@ -3,5 +3,5 @@ import { lex } from "../highlight.js";
 import { typescript } from "./typescript.js";
 
 it("matches a comment", () => {
-  expect(lex("/***/", typescript.lexers)).toEqual(["comment"]);
+  expect(lex("/***/", typescript.lexers)).toEqual([["comment"], "/***/"]);
 });
