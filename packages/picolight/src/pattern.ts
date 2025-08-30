1b7136360314b17084ed4d399101b5969e684881
Yota Toyama <raviqqe@gmail.com>
2025-08-29 23:35:12 -0700

    Fix

--- packages/picolight/src/pattern.ts
+++ packages/picolight/src/pattern.ts
@@ -3,7 +3,7 @@ export const keyword = (keywords: string[], terminal: string) =>
     `^(${keywords.map((RegExp as unknown as { escape: (text: string) => string }).escape).join("|")})(?!${terminal})`,
   );
 
-export const number = /^[+-]?[0-9.]+/;
+export const number = /^[+-]?[0-9.]+(e[0-9]+)?/i;
 
 export const string = /^"([^\\"]|\\[nrt\\"])*"/;
 
