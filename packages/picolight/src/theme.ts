b6e572a82b6a10af7a9c7244dcbebd5b59786aa1
Yota Toyama <raviqqe@gmail.com>
2026-09-08 22:30:34 -0700

    Drop tag support

--- packages/picolight/src/theme.ts
+++ packages/picolight/src/theme.ts
@@ -3,8 +3,5 @@ import type { Token } from "./token.js";
 export type Theme = {
   back: string;
   fore: string;
-  tokens: Partial<Record<Token, [tag: Tag, color: string]>>;
+  tokens: Partial<Record<Token, string>>;
 };
-
-// TODO Deprecate tags?
-export type Tag = "b" | "em" | null;
